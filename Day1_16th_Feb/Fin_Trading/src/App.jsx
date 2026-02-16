3573f010c54d3300ca656fd6f85d0dde8056dd63
Dhruv-Omnenest <Dhruv.mehta@omnenest.com>
2026-02-16 16:33:29 +0530

    folder structure

--- Day1_16th_Feb/Fin_Trading/src/App.jsx
+++ Day1_16th_Feb/Fin_Trading/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
         <Route path="/" element={<Auth />} />
         <Route path="/dash" element={<Dashboard />} />
         <Route path="/portfolio" element={<Portfolio />} />
-
       </Routes>
     </BrowserRouter>
   )
